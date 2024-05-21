e25d38c8da2dc343c8eba460602ef5304671e27e
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-05-21 22:05:46 +0530

    Shortcut to start new tab using MSYS2 shell.

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -2,5 +2,11 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 config.default_prog = {'C:/Windows/system32/wsl.exe', '-d', 'Debian'}
+config.keys = {
+  {
+    key = 'F1',
+    action = wezterm.action.SpawnCommandInNewTab {args = {'C:/msys64/msys2_shell.cmd', '-defterm', '-here', '-no-start', '-ucrt64'}},
+  },
+}
 
 return config
