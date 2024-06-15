fc728354dc11046962ab9931293bae5fd3565e9f
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-06-15 11:40:52 +0530

    Two binaries with different features and a build script.

---
+++ _/custom-prompt/build.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/sh
+
+cargo --quiet build --features=bash
+cargo --quiet build --features=zsh
--- _/custom-prompt/src/main.rs
+++ _/custom-prompt/src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
     let win = active_win_pos_rs::get_active_window().unwrap();
-    println!("{}", win.window_id);
+    println!("{} {}", f(), win.window_id);
 
 notify_rust::Notification::new()
     .summary("Firefox News")
     .body("This will almost look like a real firefox notification.")
     .show().unwrap();
+
 }
+#[cfg(feature="bash")]
+fn f() -> i32{
+0}
+#[cfg(feature="zsh")]
+fn f() -> i32{
+1}
