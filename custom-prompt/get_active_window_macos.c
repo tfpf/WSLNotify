c0003dc3109585b6049d55a12d0c09209e756f83
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-06-17 12:09:42 +0530

    macOS: return time as active window ID.
    So that two invocations of the function almost always return different results, meaning that notification control can be done in `.bash_aliases` or `.zshrc`.

--- custom-prompt/get_active_window_macos.c
+++ custom-prompt/get_active_window_macos.c
@@ -1,7 +1,10 @@
 #ifdef __APPLE__
+#include <stddef.h>
+#include <time.h>
+
 long long unsigned get_active_window_id(void)
 {
     // TODO Figure out how to do this.
-    return 0;
+    return time(NULL);
 }
 #endif
