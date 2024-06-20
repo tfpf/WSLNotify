1cf4d04328bb7be25372330a4957d5966959f566
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-06-21 00:02:31 +0530

    Formatted Objective-C source manually.

--- custom-prompt/get_active_window_id_macos.m
+++ custom-prompt/get_active_window_id_macos.m
@@ -2,13 +2,16 @@
 #include <Cocoa/Cocoa.h>
 #include <CoreGraphics/CGWindow.h>
 
-long long unsigned activeWindow(void)
+long long unsigned get_active_window_id(void)
 {
-    NSArray *windows = (NSArray *)CGWindowListCopyWindowInfo(kCGWindowListExcludeDesktopElements|kCGWindowListOptionOnScreenOnly,kCGNullWindowID);
-    for(NSDictionary *window in windows){
-        int WindowLayer = [[window objectForKey:(NSString *)kCGWindowLayer] intValue];
-        if (WindowLayer == 0) {
-            return [[window objectForKey:(NSString *)kCGWindowNumber] intValue];
+    NSArray *windows = (NSArray *)CGWindowListCopyWindowInfo(
+        kCGWindowListExcludeDesktopElements | kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
+    for (NSDictionary *window in windows)
+    {
+        int windowLayer = [[window objectForKey: (NSString *)kCGWindowLayer] intValue];
+        if (windowLayer == 0)
+        {
+            return [[window objectForKey: (NSString *)kCGWindowNumber] intValue];
         }
     }
     return 0;
